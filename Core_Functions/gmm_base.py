fixed derivative

Core_Functions/gmm_base.py
@@ -1,5 +1,6 @@
 import numpy as np
 import ot
+from scipy.linalg import fractional_matrix_power as fmp
 
 class Gaussian_Mixture:
     """
@@ -22,7 +23,7 @@ class Gaussian_Mixture:
         if np.size(w_arr) != (self.n):
             raise Exception('number of weights and components are not equal')
         else:
-            self.w = w_arr
+            self.w = w_arr / (np.sum(w_arr))
     
     def assign_m(self,m_arr):
         """
@@ -60,8 +61,14 @@ class Analytical_Gradients:
         for i in range(f.n):
             for j in range(g.n):
                 self.mean[i,j,:] = 2 * (f.m[i,:] - g.m[j,:])
-                self.cov[i,j,:,:] = (np.eye(f.d) - f.cov[i,:,:]**(-1/2) @ (f.cov[i,:,:]**(1/2) 
-                @ g.cov[j,:,:] @ f.cov[i,:,:]**(1/2))**(1/2) @ f.cov[i,:,:]**(-1/2))
+
+                # * split this into lots of parts to make sure it is right!
+                A1 = fmp(f.cov[i,:,:],-0.5)
+                A2 = fmp(f.cov[i,:,:],0.5)
+                B = g.cov[j,:,:]
+                C = fmp(A2 @ B @ A2,0.5)
+                D = A1 @ C @ A1
+                self.cov[i,j,:,:] = np.eye(f.d) - D
 
     def dmu(self,i,j):
         """
@@ -93,7 +100,11 @@ def Analytical_Wasserstein(f,g):
     #add error here if both f and g are not 1 component Gaussian_Mixture
 
     mean_dist = np.linalg.norm(f.m[0,:] - g.m[0,:])**2
-    bures = np.trace(f.cov[0,:,:] + g.cov[0,:,:] - 2 * (f.cov[0,:,:]**(1/2) @ g.cov[0,:,:] @ f.cov[0,:,:]**(1/2))**(1/2))
+    A1 = f.cov[0,:,:]
+    B = g.cov[0,:,:]
+    A2 = fmp(A1,0.5)
+    C = fmp(A2 @ B @ A2, 0.5)
+    bures = np.trace(A1 - B - 2 * C)
     W2 = mean_dist + bures
 
     return W2

Core_Functions/optimisation.py
@@ -1,5 +1,6 @@
-from gmm_base import GMM_Transport, Wasserstein_Gradients, Gaussian_Mixture
+from .gmm_base import GMM_Transport, Wasserstein_Gradients, Gaussian_Mixture
 import numpy as np
+from scipy.optimize import minimize
 
 # def Line_Search(f,g,reg,num_iter=10):
 #     f_k = f
@@ -38,14 +39,31 @@ import numpy as np
 # * To use the in-built optimisation, want to have source parameters in vector
 
 def Flattener(f):
+    """
+    Converts the Gaussian_Mixture to a 1D array of its component parameters.
+    Inputs:
+        f: mixture model to be flattened (Gaussian_Mixture) 
+    Outputs:
+        f_flat: array of mixture model parameters (array)
+        d: dimension of mixture model (int)
+    """
+    d = f.d
     w = f.w.flatten()
-    mean = f.mean.flatten()
+    mean = f.m.flatten()
     cov = f.cov.flatten()
     f_flat = np.concatenate((w,mean,cov))
-    return f_flat
+    return f_flat, d
 
 def Unflattener(f_flat,d):
-    n = np.size(f_flat) / (1 + d + d**2)
+    """
+    Inverts Flattener by converting the 1D model parameter array into a Gaussian_Mixture.
+    Inputs:
+        f_flat: flattened model parameters (array)
+        d: dimension of mixture model (int)
+    Outputs:
+        f: unflattened mixture model (Gaussian_Mixture)
+    """
+    n = np.size(f_flat) // (1 + d + d**2)
 
     f = Gaussian_Mixture(n,d)
     w = f_flat[:n]
@@ -59,9 +77,40 @@ def Unflattener(f_flat,d):
     return f
 
 def GW_Flat(f_flat,g,reg,d):
+    """
+    Determines the Gaussian-Wasserstein distance between a flattened source distribution and target Gaussian_Mixture
+    Inputs:
+        f_flat: source model parameters as flattened array (array)
+        g: target mixture (Gaussian_Mixture)
+        reg: entropic regularisation parameter (float)
+        d: dimension of the mixtures (int)
+    Returns:
+        GW: Gaussian-Wasserstein distance approximation under entropic regularisation (float)
+    """
     f = Unflattener(f_flat,d)
     # * need to then use the normal calculation that it is a Gaussian_Mixture again
+    GW = GMM_Transport(f,g,reg)[0]
     return GW
 
 def dGW_Flat(f_flat,g,reg,d):
-    return
+    """
+    Determines the gradient vector of the entropically smoothed Gaussian-Wasserstein distance.
+    Inputs:
+        f_flat: source model parameters as flattened array (array)
+        g: target mixture (Gaussian_Mixture)
+        reg: entropic regularisation parameter (float)
+        d: dimension of the mixtures (int)
+    Returns:
+        dGW_flat: derivative of Gaussian-Wasserstein with respect to each source model parameter in same order as f_flat (array)
+    """
+    f = Unflattener(f_flat,d)
+    GW, P, alpha = GMM_Transport(f,g,reg)
+    dGW_dp, dGW_dmu, dGW_dsigma = Wasserstein_Gradients(f,g,P,alpha)
+    dGW_flat = np.concatenate((dGW_dp.flatten(),dGW_dmu.flatten(),dGW_dsigma.flatten()))
+    return dGW_flat
+
+def GMM_Inversion(f,g,reg):
+    f_flat, d = Flattener(f)
+    min_flat = minimize(GW_Flat,f_flat,args=(g,reg,d),method='BFGS',jac=dGW_Flat,options={'disp': True})
+    #f_min = Unflattener(min_flat,d)
+    return min_flat

gauss_transport.py
@@ -1,36 +1,39 @@
 from Core_Functions.gmm_base import Wasserstein_Cost, Gaussian_Mixture, Analytical_Gradients, GMM_Transport, Wasserstein_Gradients
 from Core_Functions.gmm_plot import Source_Target_1D as plot1D
+from Core_Functions.optimisation import GMM_Inversion, Flattener, Unflattener
 import numpy as np
 import ot
 
 #firstly, let's consider a 1D example
 f = Gaussian_Mixture(3,1)
-g = Gaussian_Mixture(4,1)
+g = Gaussian_Mixture(3,1)
 
 f_m = np.zeros([3,1])
 f_m[:,0] = [1,3,2]
 f_c = np.zeros([3,1,1])
 f_c[:,0,0] = [0.01,0.04,0.3]
 
-g_m = np.zeros([4,1])
-g_m[:,0] = [-1,0,7,3]
-g_c = np.zeros([4,1,1])
-g_c[:,0,0] = [0.1,0.01,0.25,0.04]
+g_m = np.zeros([3,1])
+g_m[:,0] = [-1,7,3]
+g_c = np.zeros([3,1,1])
+g_c[:,0,0] = [0.1,0.01,0.04]
 
 f.assign_w(np.array([0.3,0.2,0.5]))
 f.assign_m(f_m)
 f.assign_cov(f_c)
 
-g.assign_w(np.array([0.1,0.3,0.4,0.2]))
+g.assign_w(np.array([0.3,0.4,0.3]))
 g.assign_m(g_m)
 g.assign_cov(g_c)
 
 
-#W = Wasserstein_Cost(f, g)
-#P, log = ot.bregman.sinkhorn_stabilized(f.w, g.w, W, 0.001,log=True)
-# print(log['logu'],log['logv'])
+W = Wasserstein_Cost(f, g)
+P, log = ot.bregman.sinkhorn_stabilized(f.w, g.w, W, 0.001,log=True)
+#print(log['logu'],log['logv'])
 
-#GW, P, alpha = GMM_Transport(f,g,0.01)
-#print(Wasserstein_Gradients(f,g,P,alpha))
+GW, P, alpha = GMM_Transport(f,g,0.01)
+print(Wasserstein_Gradients(f,g,P,alpha))
 
-fig, ax = plot1D(f,g,[-3,4],res=300)
+#fig, ax = plot1D(f,g,[-3,4],res=300)
+
+#print(GMM_Inversion(f,g,0.01))